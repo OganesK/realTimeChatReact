4ffca5d63a19c73467b96a82da0a57070d0c7a95
OganesK <53556592+OganesK@users.noreply.github.com>
2021-09-08 13:56:39 +0300

    prisma added

--- server/config/config.js
+++
@@ -1,8 +0,0 @@
-const config =  
-{
-    BASE_DB_URL: "mongodb+srv://kostjaog:qwertyt123e5@cluster0.dp8zu.mongodb.net/ReactChat?retryWrites=true&w=majority",
-    PORT: 3001,
-    NODE_ENV: 'production',
-    DB_UPDATE: false,
-}
-export default config;
